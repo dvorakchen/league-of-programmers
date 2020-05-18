3ce3af7ea83d58101cd671cda017aa4ade8ffe3e
mrunowen <birenchens@outlook.com>
2020-05-18 21:54:12 +0800

    modify loading

--- src/League-Of-Programmers/ClientApp/src/app/routers.module.ts
+++ src/League-Of-Programmers/ClientApp/src/app/routers.module.ts
@@ -7,9 +7,9 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 const routes: Routes = [
-  //{ path: '', component: HomeComponent, pathMatch: 'full' },
-  //{ path: 'counter', component: CounterComponent },
-  //{ path: 'fetch-data', component: FetchDataComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
 ];
 
 @NgModule({
