911d39a3778d1b9ad586d27b6b32f8f074311095
mrunowen <birenchens@outlook.com>
2020-06-04 22:09:44 +0800

    modify user client

---
+++ src/League-Of-Programmers/ClientApp/src/app/shared/user-info-box/user-info-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-box',
+  templateUrl: './user-info-box.component.html',
+  styleUrls: ['./user-info-box.component.sass']
+})
+export class UserInfoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
