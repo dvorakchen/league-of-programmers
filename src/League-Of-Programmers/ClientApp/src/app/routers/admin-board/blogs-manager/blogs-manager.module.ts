ec3f309298d57f386f372a24abe6ad563bc151d8
mrunowen <birenchens@outlook.com>
2020-06-16 21:47:17 +0800

    add blog detail in administrator

--- src/League-Of-Programmers/ClientApp/src/app/routers/admin-board/blogs-manager/blogs-manager.module.ts
+++ src/League-Of-Programmers/ClientApp/src/app/routers/admin-board/blogs-manager/blogs-manager.module.ts
@@ -5,12 +5,14 @@ import { BlogsManagerRoutingModule } from './blogs-manager-routing.module';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [ListComponent, DetailComponent],
   imports: [
     SharedModule,
-    BlogsManagerRoutingModule
+    BlogsManagerRoutingModule,
+    MarkdownModule.forChild()
   ]
 })
 export class BlogsManagerModule { }
