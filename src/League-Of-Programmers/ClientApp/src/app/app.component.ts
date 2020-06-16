9f798af8c157e25bf530607f0032769617ec0e51
myfor <mrmyfor@outlook.com>
2020-06-16 09:10:35 +0800

    add footer in client page

--- src/League-Of-Programmers/ClientApp/src/app/app.component.ts
+++ src/League-Of-Programmers/ClientApp/src/app/app.component.ts
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  year = new Date().getFullYear();
 }
--- src/League-Of-Programmers/ClientApp/src/app/app.module.ts
+++ src/League-Of-Programmers/ClientApp/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+//  import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RoutesModule } from './routers.module';
 
 import { HTTP_INTERCEPTOR_PROVIDERS } from './interceptors/barrel';
 
 import { MaterialModule } from './shared/material.module';
+import { SharedModule } from './shared/shared.module';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { AppComponent } from './app.component';
@@ -22,10 +23,11 @@ import { NavMenuComponent } from './routers/nav-menu/nav-menu.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule,
+    //  FormsModule,
     RoutesModule,
     MaterialModule,
-    MarkdownModule.forRoot()
+    MarkdownModule.forRoot(),
+    SharedModule
   ],
   providers: [HTTP_INTERCEPTOR_PROVIDERS],
   bootstrap: [AppComponent]
